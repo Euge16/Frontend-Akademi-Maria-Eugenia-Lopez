dc87a725b31c2b3ef46ff92ff886ec3394a540cf
Euge16 <71157100+Euge16@users.noreply.github.com>
2025-04-01 15:27:36 -0300

    Reestructuracion del proyecto: - Elimine archivos por defecto de React que no se utilizan - Modifique configuraciones de package.json - Añadi carpetas para trabajar con React Redux

---
+++ gestion-productos/src/components/App.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+
+// Prueba para mostrar mis productos
+function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/products')
+      .then(response => response.json())
+      .then(data => setProducts(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Productos</h1>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            {product.name} - ${product.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
+
